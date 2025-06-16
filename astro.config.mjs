e3214e8fb6eddc2121032035a363a32aef0d1336
issa <issa.zaytoun@googlemail.com>
2025-06-16 15:14:34 +0200

    static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://ihr-gartenbau.de'
+  integrations: [tailwind(), sitemap()],
+  output: 'static',
+  site: 'https://YOURUSERNAME.github.io',
+  base: '/julian_GB_V3',
+  build: {
+    assets: 'assets'
+  }
 });
